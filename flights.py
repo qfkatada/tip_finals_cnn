96447f4d8638fc0ff0818c05c4685da141ea7b5b
fkatada <frank@yuta.ph>
2024-05-18 16:37:16 +0800

    origin master

--- flights.py
+++ flights.py
@@ -1,3 +1,6 @@
+import datetime
+import decimal
+from typing import Dict
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -58,19 +61,25 @@ st.write("""
     ### Primary Details
     """     
 )
+payload:Dict = {}
 
 # Month Day Year
 departure_date = st.date_input(label="Departure Date",
                                help="Date of Departure"
                                )
 
+payload['month']= int(departure_date.month)
+payload['day']= int(departure_date.day)
+
 # scheduled departure time
 scheduled_departure_time = st.time_input( label="Scheduled Departure Time", help="Flight Scheduled Departure Time")
+payload['hour']= int(scheduled_departure_time.hour)
+payload['scheduled departure time'] = int((scheduled_departure_time.hour*100) + (scheduled_departure_time.minute *10))
 
 # departure time
 departure_time = st.time_input(label="Time Departed", help="Actual time the plane departed")
-
-# departure_delay = scheduled_departure_time - departure_time
+payload['departure time'] = int((departure_time.hour*100) + (departure_time.minute *10))
+payload['departure delay'] = payload['departure time'] - payload['scheduled departure time']
 
 # distance = origin + distination
 
@@ -83,64 +92,75 @@ origin = st.selectbox(
 
 'Airport Origin: ', origin
 
+payload['origin'] = origin
+
+# reg_av_ds[['name', 'carrier']].value_counts().reset_index().set_index('name').loc['United Air Lines Inc.', 'carrier']
+airline_bucket = df[['name', 'carrier']].value_counts().reset_index().set_index('name')
+
 # Dropdown
-airlines = st.selectbox(
+# airlines
+airlines_name = st.selectbox(
     key=2,
     label='Airlines',
     options=df['name'].unique(),
     )
 
-'Airlines: ', airlines
+carrier = airline_bucket.loc[airlines_name, 'carrier']
+# 'Airlines: ', airlines, 
+'Carrier: ', carrier
 
-carrier = st.selectbox(
-    key=3,
-    label='Carrier Type',
-    options=df['carrier'].unique(),
-    )
-
-'Carrier Type: ', airlines
+payload['name'] = airlines_name
+payload['carrier'] = carrier
 
 # Dropdown
 flight = st.selectbox(
         key=4,
-        label='Airport Destination',
+        label='Flight Number',
         options=df['flight'].unique(),
     )
 
 'Flight: ', flight
+payload['flight'] = flight
 
 st.write("""
     ### Destination
     """     
 )
 
-# Dropdown
-destination = st.selectbox(
+airport_destination_bucket = df[['destination', 'airport', 'municipality', 'elevation']].value_counts().reset_index().set_index('airport')
+
+# Dropdown - Automap airport_destination,destination, municipality, elavation
+airport = st.selectbox(
         key=5,
         label='Airport Destination',
-        options=df['destination'].unique(),
+        options=df['airport'].unique(),
     )
+municipality = airport_destination_bucket.loc[airport, 'municipality']
+destination = airport_destination_bucket.loc[airport, 'destination']
+elevation = airport_destination_bucket.loc[airport, 'elevation']
+'Municipality: ', municipality, destination
+'Elevation: ',  elevation
 
-# Automap airport_destination, municipality, elavation
-#
-# airport_destination = st.selectbox(
-#         key=3,
-#         label='Airport Destination',
-#         options=df['airport'].unique(),
-#     )
-
-# 
+payload['municipality'] = municipality
+payload['destination'] = destination
+payload['elevation'] = elevation
 
 # scheduled departure time
 scheduled_arrival_time = st.time_input(label="Arrival Time", help="The Scheduled Arrival time for the destination or connecting Airport")
+payload['scheduled arrival time'] = scheduled_arrival_time
 
 # Default Values
 described_values = df[['humidity','pressure', 'temperature', 'wind direction', 'wind speed', 'distance', 'eta duration']].describe()
 
 eta_duration = st.number_input(
     label="Estimated Flight Dration", 
-    min_value=described_values.loc['min','eta duration'], 
+    # min_value=described_values.loc['min','eta duration'], 
+    min_value=0.00, 
+    max_value=described_values.loc['max', 'eta duration'],
+    value=described_values.loc['mean', 'eta duration'],
     help="ETA Flight Duration from the ticket")
+
+payload['eta duration'] = eta_duration
 
 st.write("""
     ## Weather Forecast
@@ -150,10 +170,12 @@ st.write("""
 humidity = st.slider(
     key=6,
     label='Humidity',
-    min_value=(described_values.loc['min', 'pressure'] - described_values.loc['std', 'pressure']), 
-    max_value=(described_values.loc['max', 'pressure'] + described_values.loc['std', 'pressure']), 
-    value=described_values.loc['mean', 'pressure']
+    min_value=(described_values.loc['min', 'humidity'] - described_values.loc['std', 'humidity']), 
+    max_value=(described_values.loc['max', 'humidity'] + described_values.loc['std', 'humidity']), 
+    value=described_values.loc['mean', 'humidity']
 )
+
+payload['humidity'] = humidity
 
 pressure = st.slider(
     key=7,
@@ -163,6 +185,8 @@ pressure = st.slider(
     value=described_values.loc['mean', 'pressure']
 )
 
+payload['pressure'] = pressure
+
 temperature = st.slider(
     key=8,
     label='Temperature',
@@ -170,6 +194,8 @@ temperature = st.slider(
     max_value=(described_values.loc['max', 'temperature'] + described_values.loc['std', 'temperature']), 
     value=described_values.loc['mean', 'temperature']
 )
+
+payload['temperature'] = temperature
 
 wind_direction = st.slider(
     key=9,
@@ -179,6 +205,8 @@ wind_direction = st.slider(
     value=described_values.loc['mean', 'wind direction']
 )
 
+payload['wind direction'] = wind_direction
+
 wind_speed = st.slider(
     key=10,
     label='Wind Speed',
@@ -187,6 +215,8 @@ wind_speed = st.slider(
     value=described_values.loc['mean', 'wind speed']
 )
 
+payload['wind speed'] = wind_speed
+
 weather_description = st.selectbox(
         key=11,
         label='Weather Description',
@@ -194,16 +224,56 @@ weather_description = st.selectbox(
         index=10
     )
 
+payload['weather description'] = weather_description
+
 st.write("""
     ## Possibility of missing a connecting flight due to arrival delay
     """     
 )
 
-predict = st.button("Predict", type="primary")
-print(predict)
-# if st.button("Predict"):
-#     # Test Predict
-#     pass
+
+if st.button("Predict",  type="primary"):
+    _payload:Dict = {
+        'month': departure_date.month,
+        'day': departure_date.day,
+        'hour': departure_time.hour,
+        'origin': origin,
+        'scheduled departure time': float(scheduled_departure_time.hour + (scheduled_departure_time.minute *100)),
+        'departure time': 517.0,
+        'departure delay': 2.0,
+        'distance': 1400,
+        'destination': 'IAH',
+        'scheduled arrival time': 819,
+        'eta duration': 304,
+        'humidity': 81.0,
+        'pressure': 1025.0,
+        'temperature': 281.49,
+        'weather description': 'broken clouds',
+        'wind direction': 90.0,
+        'wind speed': 4.0,
+        'name': 'United Air Lines Inc.',
+        'carrier': 'UA',
+        'flight': 1545,
+        'airport': 'George Bush Intercontinental Houston Airport',
+        'municipality': 'Houston',
+        'elevation': 97.0,
+        # 'total travel delay': 13.0
+    }
+
+    _orig = list(_payload.keys())
+    _proc = list(payload.keys())
+
+    print("-"* 100)
+    print(f"[+] original {len(_orig)}: {_orig}")
+    print(f"[+] payload {len(_proc)}: {_proc}")
+    print("-"* 100)
+
+    for k in list(payload.keys()):
+        if k in _orig:
+           continue
+        else:
+            print(k)
+    # print(f"[+] {payload}")
 
 st.divider()
 st.write("""
